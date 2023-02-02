c52f9958dd5c684a2d02ba1b7c9cd6c2be62768d
jzlbornoz <javieraap10@gmail.com>
2023-02-02 15:55:31 -0400

    [Rutas y estilos]

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from '../styles/Navigation.module.css';
+
+const links = [{
+    label: 'home',
+    root: '/',
+},
+{
+    label: 'about',
+    root: '/about'
+}]
+
+const Navigation = () => {
+    return (
+        <header className={styles.Header}>
+            <nav>
+                <ul className={styles.Navigation}>
+                    {links.map(({ label, root }) => (
+                        <li key={label}>
+                            <Link href={root}>{label}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export { Navigation } 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
-import './globals.css'
+import { Navigation } from "./components/Navigation"
+import './styles/globals.css';
+
+
 
 export default function RootLayout({
   children,
@@ -6,13 +9,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html>
+      <head>
+        <title>MyApp</title>
+      </head>
+      <body>
+        <Navigation />
+        {children}
+      </body>
     </html>
   )
 }
