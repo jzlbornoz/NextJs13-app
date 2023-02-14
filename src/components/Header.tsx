0d9ca510bc6d1ff85f43e88f0b57c70cc57cf8d8
jzlbornoz <javieraap10@gmail.com>
2023-02-13 22:42:46 -0400

    [add] fa icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import React from 'react'
 import styles from '../styles/components/Header.module.css';
 
 
 const Header = () => {
-    
+
 
     return (
         <header className={styles.Header}>
-            <h2>Menu</h2>
+            <FontAwesomeIcon icon={faBars} style={{ 'width': '1.5em' }} />
             <div className={styles.HeaderUser}>
+                <FontAwesomeIcon icon={faUserCircle} />
             </div>
         </header>
     )
